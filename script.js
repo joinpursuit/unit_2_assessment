4f402f0326c5b829a586730988ad10248632297c
MikePursuit <michaelamparo@pursuit.org>
2019-09-22 16:31:27 -0400

    completed

---
+++ script.js
@@ -0,0 +1,54 @@
+const loadMovieInfo = (name) => {
+    let filmObj = filmsObject[name];
+    let title = document.createElement('h3');
+    let year = document.createElement('p');
+    let description = document.createElement('p');
+    let filmArr = [title, year, description];
+    
+    title.innerText = filmObj.title;
+    year.innerText = filmObj.release_date;
+    description.innerText = filmObj.description;
+    filmArr = [title, year, description];
+    
+    document.querySelector('#movie_info').innerText = '';
+    filmArr.forEach(item => document.querySelector('#movie_info').appendChild(item));
+    
+    return filmArr;
+}
+
+
+const addReview = () => {
+    let review = document.createElement('li');
+    review.innerHTML = `<strong>${document.querySelector('h3').innerText}:</strong> ${document.querySelector('input').value}`;
+    document.querySelector('input').value = '';
+    document.querySelector('ul').appendChild(review);
+}
+
+
+const createFilmOption = (name) => {
+    let option = document.createElement('option');
+
+    option.value = name;
+    option.innerText = name;
+    document.querySelector('select').appendChild(option);
+    
+    return option;
+}
+
+
+let filmsObject = {};
+const fetchMovies = async () => {
+    let films = await axios.get(`https://ghibliapi.herokuapp.com/films`);
+    let filmsArr = films.data;
+    let filmsObj = {};
+    
+    filmsArr.forEach(film => filmsObj[film.title] = film);
+    filmsArr.forEach(film => createFilmOption(film.title));
+    filmsObject = filmsObj;
+    loadMovieInfo(filmsArr[0].title);
+
+    return filmsObj;
+}
+
+
+fetchMovies();
