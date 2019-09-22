6cf46df4aa8a685dced5bbf02358f6f74ef12563
sergiocohens <sergiocohensalama@pursuit.org>
2019-09-22 14:44:05 -0400

    assessment

---
+++ index.js
@@ -0,0 +1,65 @@
+document.addEventListener('DOMContentLoaded', () => {
+  getMovieNames()
+})
+
+const getMovieNames = () => {
+  fetch("https://ghibliapi.herokuapp.com/films")
+  .then(response => response.json())
+  .then(movies => {
+    for(i = 0; i < movies.length; i ++) {
+      let list = document.querySelector('select')
+      let movie = document.createElement('option')
+      movie.text = movies[i].title
+
+      list.appendChild(movie)
+    }
+  })
+}
+
+const displayMovieInfo = () => {
+  let info = document.getElementById('movie')
+  info.innerText = ''
+
+  let list = document.querySelector('select')
+  let name = list.value
+
+  fetch("https://ghibliapi.herokuapp.com/films")
+  .then(response => response.json())
+  .then(movies => {
+    for(i = 0; i < movies.length; i++) {
+      if(movies[i].title === name) {
+        let info = document.getElementById('movie')
+        let title = document.createElement('h3')
+        let year = document.createElement('p')
+        let description = document.createElement('p')
+        title.innerText = movies[i].title
+        year.innerText = movies[i].release_date
+        description.innerText = movies[i].description
+
+        info.appendChild(title)
+        info.appendChild(year)
+        info.appendChild(description)
+      }
+    }
+  })
+}
+
+const addReview = () => {
+  let title = document.querySelector('h3')
+  let reviews = document.querySelector('ul')
+  let review = document.createElement('li')
+  let input = document.createElement('p')
+  review.innerText = title.innerText + ': '
+  review.style.fontWeight = 'bold'
+
+  reviews.appendChild(review)
+
+  let textbox = document.getElementById('textbox')
+  let span = document.createElement('span')
+  span.innerText = textbox.value
+  span.style.fontWeight = 'normal'
+
+  review.appendChild(span)
+
+  textbox.value = ''
+}
