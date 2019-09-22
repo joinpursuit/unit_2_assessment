c64042712072025f548cc0b16ab00520b6baa116
tawnyblvd <margara.sees@gmail.com>
2019-09-22 11:35:58 -0400

    first review of user text entry and adding li

--- unit2Assessment.js
+++ unit2Assessment.js
@@ -1,14 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
   // console.log("just checking on things so far");
-  submitButtonForReviews();
+  //does this submit button function actually go here? check back after fetch
+  whenSubmitButtonIsClicked();
 })
 
-const submitButtonForReviews = () => {
+
+
+const whenSubmitButtonIsClicked = () => {
   //pull our button and say when clicked perform submitting function
   const submitButton = document.querySelector("#submitButton");
-  submitButton.addEventListener("click", submittingReviewsToPage);
+  submitButton.addEventListener("click", addUserReviewToList);
 }
 
-const submittingReviewsToPage = () => {
+const addUserReviewToList = () => {
+  //this line says pull the ul using its id
+  //and set it to a variable so we can reference it
+  //should the userReviewsList container be in here or outside a function?
+  let userReviewsList = document.querySelector("#reviewsListContainer");
 
+  //create a new li
+  let newReview = document.createElement("li");
+  //add an li to the review list ul
+  userReviewsList.appendChild(newReview);
+
+  //li's innerText should have the text entered in my input text box
+  newReview.innerText = userReviewEntry.innerText;
 }
+
+/*
+so
+
+
+
+*/
