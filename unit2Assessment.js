ba0052210ef861fdbaf5a77c96a157552b023867
tawnyblvd <margara.sees@gmail.com>
2019-09-22 11:18:50 -0400

    set up a listener for my review submit button

--- unit2Assessment.js
+++ unit2Assessment.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("just checking on things so far");
+  // console.log("just checking on things so far");
+  submitButtonForReviews();
 })
+
+const submitButtonForReviews = () => {
+  //pull our button and say when clicked perform submitting function
+  const submitButton = document.querySelector("#submitButton");
+  submitButton.addEventListener("click", submittingReviewsToPage);
+}
+
+const submittingReviewsToPage = () => {
+
+}
