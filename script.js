b7f0dff02cf5589fa66fc07500c5a875e16966af
MikePursuit <michaelamparo@pursuit.org>
2019-09-22 16:52:36 -0400

    added default option

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 const loadMovieInfo = (name) => {
+    if (!name) {
+        document.querySelector('#movie_info').innerHTML = `<h3>To begin select a movie</h3>`;
+        document.querySelector('form').style.display = 'none';
+        return;
+    }
     let filmObj = filmsObject[name];
     let title = document.createElement('h3');
     let year = document.createElement('p');
     let description = document.createElement('p');
     let filmArr = [title, year, description];
     
+    
     title.innerText = filmObj.title;
     year.innerText = filmObj.release_date;
     description.innerText = filmObj.description;
     filmArr = [title, year, description];
     
+    document.querySelector('form').style.display = 'block';
     document.querySelector('#movie_info').innerText = '';
     filmArr.forEach(item => document.querySelector('#movie_info').appendChild(item));
     
@@ -45,7 +52,6 @@ const fetchMovies = async () => {
     filmsArr.forEach(film => filmsObj[film.title] = film);
     filmsArr.forEach(film => createFilmOption(film.title));
     filmsObject = filmsObj;
-    loadMovieInfo(filmsArr[0].title);
 
     return filmsObj;
 }
