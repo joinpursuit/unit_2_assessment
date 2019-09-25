68ae212bb364234a6a50b87cce54f010dd329d5a
AminesCodes <aminebensalem@pursuit.org>
2019-09-25 17:05:15 -0400

    added a fteature of displaying reviews related to the selected movie

--- index.js
+++ index.js
@@ -3,22 +3,38 @@ const baseURL = "https://ghibliapi.herokuapp.com/films"
 document.addEventListener("DOMContentLoaded", async () => {
     let allMoviesData = await onlineRequest(baseURL);
     let selectedMovie;
+    let specificReviewsSelected = false;
 
-    addMoviesTitlesToSelections (allMoviesData);
+    addMoviesTitlesToSelections(allMoviesData);
 
     let form = document.querySelector("form");
     form.style.visibility = "hidden";
+    
+    let reviewContainer = document.querySelector("#reviewContainer");
+    reviewContainer.style.visibility = "hidden";
+
+    let reviewUl = document.querySelector("#reviews");
+    
+    let specificReviewUl = document.querySelector("#specificReviews");
+    specificReviewUl.style.display = "none";
 
     let selectMenu = document.querySelector("#selectBox");
     selectMenu.addEventListener("change", async () => {
+        console.log(specificReviewsSelected)
         if (selectMenu.value === "") {
             selectedMovie = null;
             form.style.visibility = "hidden";
-        } else {
-            let movieURL = `${baseURL}/${selectMenu.value}`;
-            selectedMovie = await onlineRequest(movieURL);
+            reviewContainer.style.visibility = "hidden";
+        } else {                                                 // I HAD TO SWITCH THE selectedMenu.value FROM HOLDING THE MOVIE ID TO THE INDEX OF THE MOVIE
+            // let movieURL = `${baseURL}/${selectMenu.value}`;  THIS IS HOW TO MAKE A NEW NETWORK REQUEST TO HAVE ALL INFORMATIONS RELATIVE TO THE SELECTED MOVIE
+            // selectedMovie = await onlineRequest(movieURL);    BUT I FOUND A BETTER WAY TO GET THESE INFORMATIONS 
+            selectedMovie = allMoviesData[selectMenu.value];
             form.style.visibility = "visible";
+            reviewContainer.style.visibility = "visible";
+
+            manageDisplayOfReviews(specificReviewsSelected, specificReviewUl, reviewUl, selectedMovie);
         }
+        
 
         addMovieToDOM(selectedMovie);
 
@@ -41,10 +57,26 @@ document.addEventListener("DOMContentLoaded", async () => {
             newSpan.innerText = reviewInput.value;
             newReview.appendChild(newSpan);
             reviewsContainer.appendChild(newReview);
+            
+            addReviewToSelectedMovie(selectedMovie, reviewInput.value);
             reviewInput.value = "";
-        }
-    })
 
+            if (specificReviewsSelected) {
+                addSpecificReviewsToDom(specificReviewUl, selectedMovie)
+            }
+        }
+    }) // END OF EVENT LISTNER FOR FORM
+
+    let checkbox = document.querySelector("input[type='checkbox']");
+    checkbox.addEventListener("click", () => {
+        if (checkbox.checked) {
+            specificReviewsSelected = true;
+        } else {
+            specificReviewsSelected = false;
+        }
+
+        manageDisplayOfReviews(specificReviewsSelected, specificReviewUl, reviewUl, selectedMovie);
+    })
 
 }) // DOM CONTENT LOADED
 
@@ -59,10 +91,10 @@ const onlineRequest = async (url) => {
 const addMoviesTitlesToSelections = (fullMoviesInfo) => {
     let selectMenu = document.querySelector("#selectBox");
     
-    for (let movie of fullMoviesInfo) {
+    for (let i = 0; i < fullMoviesInfo.length; i++) {
         let newOption = document.createElement("option");
-        newOption.value = movie.id;
-        newOption.innerText = movie.title;
+        newOption.value = i;
+        newOption.innerText = fullMoviesInfo[i].title;
         selectMenu.appendChild(newOption);
     }
 }
@@ -81,5 +113,35 @@ const addMovieToDOM = (movie) => {
         title.innerText = "";
         year.innerText = "";
         description.innerText = "";
+    }
+}
+
+const addReviewToSelectedMovie = (movie, review) => {
+    if (!movie.reviews) {
+        movie.reviews = [];
+    }
+    movie.reviews.push(review);
+}
+
+const manageDisplayOfReviews = (spReview, spReviewParent, mixedReviews, movie) => {
+    if (spReview) {
+        spReviewParent.style.display = "block";
+        mixedReviews.style.display = "none";
+        addSpecificReviewsToDom(spReviewParent, movie);
+    } else {
+        spReviewParent.style.display = "none";
+        mixedReviews.style.display = "block";
+    }
+}
+
+const addSpecificReviewsToDom = (parent, movie) => {
+    parent.innerText = "Reviews :";
+
+    if (movie.reviews) {
+        for (let review of movie.reviews) {
+            let newLi = document.createElement("li");
+            newLi.innerText = review;
+            parent.appendChild(newLi)
+        }
     }
 }
