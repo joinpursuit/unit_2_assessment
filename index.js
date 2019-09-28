f3b584797d5262f6a7764dac561ae0e1b8b25585
your name here <your_email_here>
2019-09-27 21:07:20 -0400

    Savita Unit2 2nd attempt

--- index.js
+++ index.js
@@ -10,56 +10,84 @@ document.addEventListener('DOMContentLoaded', () => {
         let li = document.createElement("li")
         li.innerText = review.value
         list.appendChild(li);
+    })
 
+    const selectElement = document.querySelector('#select');
+
+    selectElement.addEventListener('change', (event) => {
+        const result = document.querySelector('#main');
+        const value =  event.target.value;
+        cleanMain()
+        getOneFilm(value)
+        
     })
 
 })
 
-
-
-
 const getFilm = () => {
-    const select = document.getElementById('select')
-    const option = document.createElement('option')
-    option.classList.add("option")
-    option.appendChild(document.createTextNode('Text'));
-    option.value = 'value';
-    select.appendChild(option);
-
+    
     const url = "https://ghibliapi.herokuapp.com/films"
     fetch(url)
         .then(response => response.json())
         .then(films => {
-
+                console.log(films);
+                console.log();
+                
             for (let i = 0; i < films.length; i++) {
-                let film = films[i].title
-                option.innerHTML = films[0].title
-                displayMovieInfo(films[i])
+                let title = films[i].title
+                let filmId = films[i].id
+                displayTitleOption(title, filmId)
+
+                
 
             }
 
         })
 }
 
+const getOneFilm = (value) => {
+       
+    const url = `https://ghibliapi.herokuapp.com/films/${value}`
+    fetch(url)
+    .then(response => response.json())
+    .then(film => {
+        displayMovieInfo(film)
 
 
-const displayMovieInfo = (films) => {
+})
+}
 
+
+const displayTitleOption = (title, filmId) => {
+    const select = document.getElementById('select')
+    const option = document.createElement('option')
+    option.classList.add("option")
+    option.appendChild(document.createTextNode('Text'));
+    option.value = filmId;
+    select.appendChild(option);
+    option.innerHTML = title
+    
+}
+
+const displayMovieInfo = (film) => {
+    
     const main = document.getElementById('main')
     const titles = document.createElement('h2')
-    titles.innerText = films.title;
+    titles.innerText = film.title;
     main.appendChild(titles)
 
     let year = document.createElement("h3")
-    year.innerText = films.release_date
+    year.innerText = film.release_date
     main.appendChild(year)
 
     let para = document.createElement("p")
-    para.innerText = films.description
+    para.innerText = film.description
     main.appendChild(para)
+}
 
-
-
+const cleanMain = () => {
+    const main = document.getElementById("main")
+    main.innerHTML = ""
 }
 
 
