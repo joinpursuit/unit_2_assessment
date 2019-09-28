de13c36d4452d9e5bf0a623cb82131cb752c6a7b
Scripter12 <elicardona57@gmail.com>
2019-09-28 16:42:44 -0400

    hide input and submit button when selecting empty option

--- index.js
+++ index.js
@@ -1,64 +1,72 @@
-document.addEventListener("DOMContentLoaded",()=>{
+document.addEventListener("DOMContentLoaded", () => {
+  
+  fetch("https://ghibliapi.herokuapp.com/films")
+    .then(response => response.json())
+    .then(response => {
+      getFilms(response)
+    })
 
-fetch("https://ghibliapi.herokuapp.com/films")
-.then(response => response.json())
-.then(response => {
-  getFilms(response)
-})
+  let select = document.querySelector("select")
+  select.addEventListener("change", (e) => {
+    let words = document.querySelector("#words")
+    let submit = document.querySelector("#sub")
+    if (!e.target.value){
+      words.style.display = "none"
+      submit.style.display = "none"
+    }else{
+      words.style.display = "inline"
+      submit.style.display = "inline"
+    }
+    words.value = ""
+    let info = document.querySelector("#info")
+    let index = arrjson[e.target.selectedIndex]
+    fetch(`https://ghibliapi.herokuapp.com/films/${index}`)
+      .then(response => response.json())
+      .then(response => {
+        while (info.firstChild) {
+          info.removeChild(info.firstChild);
+        }
+        let title = document.createElement("h3")
+        let releaseYear = document.createElement("p")
+        let description = document.createElement("p")
+        title.innerText = response.title;
+        releaseYear.innerText = response.release_dateb
+        description.innerText = response.description
+        if (index === null) {
+          title.innerText = "";
+          releaseYear.innerText = ""
+          description.innerText = ""
+        }
+        info.appendChild(title)
+        info.appendChild(releaseYear)
+        info.appendChild(description)
+      })
 
-let select = document.querySelector("select")
-select.addEventListener("change",(e)=>{
-  let words = document.querySelector("#words")
-  words.value = ""
-  let info = document.querySelector("#info")
-  let index = arrjson[e.target.selectedIndex]
-  fetch(`https://ghibliapi.herokuapp.com/films/${index}`)
-.then(response => response.json())
-.then(response => {
-  while(info.firstChild){
-    info.removeChild(info.firstChild);
-  }
-  let title = document.createElement("h3")
-  let releaseYear = document.createElement("p")
-  let description = document.createElement("p")
-  title.innerText = response.title;
-  releaseYear.innerText = response.release_date
-  description.innerText = response.description
-  if(index === null){
-    title.innerText = "";
-    releaseYear.innerText = ""
-    description.innerText = ""
-  }
-info.appendChild(title)
-info.appendChild(releaseYear)
-info.appendChild(description)
-})
+  })
 
-})
-
-let submit = document.querySelector("#sub")
-submit.addEventListener("click",(e)=>{
-e.preventDefault()
-postReview()
-})
+  let submit = document.querySelector("#sub")
+  submit.addEventListener("click", (e) => {
+    e.preventDefault()
+    postReview()
+  })
 
 })
 
 let arrjson = [null];
 
-function getFilms(film){
+function getFilms(film) {
   let select = document.querySelector("select")
-  for(let i of film){
-  let option = document.createElement("option")
-  option.innerText = i.title;
-  select.appendChild(option)
-  arrjson.push(i.id)
+  for (let i of film) {
+    let option = document.createElement("option")
+    option.innerText = i.title;
+    select.appendChild(option)
+    arrjson.push(i.id)
   }
 }
 
 
 
-function postReview(){
+function postReview() {
   let words = document.querySelector("#words")
   let list = document.querySelector("ul")
   let t = document.querySelector("h3")
@@ -68,9 +76,9 @@ function postReview(){
   review.appendChild(bold)
   let text = document.createTextNode(`: ${words.value}`)
   review.appendChild(text);
-  
-  if(t.innerText && words.value){
-  list.appendChild(review)
+
+  if (t.innerText && words.value) {
+    list.appendChild(review)
   }
   words.value = ""
 }
