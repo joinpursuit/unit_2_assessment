32e6f7e0d177279aee05cec9ee273878910b129b
Owen Jones <owenthefirst93@gmail.com>
2019-09-29 18:03:37 -0400

    added random button and fixed functions

--- movie-app1.js
+++ movie-app1.js
@@ -2,66 +2,51 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const selector = document.querySelector('#select-movie')
     const form = document.querySelector('#form')
-    const mainDiv = document.querySelector('#maincontent')
+    const randoButton = document.querySelector('#randomize-movie')
    
-    const grabMovies = async () => {
-        const movieRequest = await axios.get(mainURL)
-        const movieInfo = movieRequest.data
-        console.log(movieInfo)
-        createMovieOptions(movieInfo)
-    }
-   /*
-   
-   const movieData = grabMovies()
-   createMovieOptions(movieData)
-   */
    grabMovies()
 
     selector.addEventListener('change', () => {
-       if (mainDiv.innerHTML === '') {
-           displayMovieContent(selector.selectedIndex - 1)
-       } else if (mainDiv.innerHTML !== '') {
-           mainDiv.innerHTML = ''
-           displayMovieContent(selector.selectedIndex - 1)
-       }
+        changeMovieContent()
+        displayMovieContent(selector.selectedIndex - 1)
     }) 
 
     form.addEventListener('submit', postReview)
-    
+
+    randoButton.addEventListener('click',() => {
+        changeMovieContent()
+        let newIndex = randomizeMovie()
+        displayMovieContent(newIndex)
+        selector.selectedIndex = newIndex + 1
+    })
 })
 
 const mainURL = "https://ghibliapi.herokuapp.com/films"
 
-// const createMovieOptions = (movies) => {
-//     const selector = document.querySelector('#select-movie')
-//     // let index = 1
-//     for (let i = 0; i < movies.length; i++){
-//         const option = document.createElement('option')
-//         option.text = movies[i].title
-//         option.id = i
-//         selector.add(option)
-//         // index++
-//     }
-// }
+const grabMovies = async () => {
+    const movieRequest = await axios.get(mainURL)
+    const movieInfo = movieRequest.data
+    console.log(movieInfo)
+    createMovieOptions(movieInfo)
+}
 
 const createMovieOptions = (movies) => {
     movies.map(renderList)
-    // let index = 1
-    // for (let i = 0; i < movies.length; i++){
-  
-    //     index++
-    // }
 }
 
 const renderList = (element, index) => {
 
     const option = document.createElement('option')
     option.text = element.title
-    option.id = index
+    option.id = index 
     document.querySelector('#select-movie').add(option)
 }
 
-
+const changeMovieContent = () => { 
+     mainDiv = document.querySelector('#maincontent') 
+    mainDiv.innerHTML = ''
+}
+ 
 const displayMovieContent = async (optionId) => {
 
     const movieRequestB = await axios.get(mainURL)
@@ -69,7 +54,9 @@ const displayMovieContent = async (optionId) => {
     
     const mainDiv = document.querySelector('#maincontent')
     const movieDiv = document.createElement('div')
-    const createHeading = document.createElement('h5')
+    movieDiv.id = 'movie-div'
+
+    const createHeading = document.createElement('h3')
     const createYearReleased = document.createElement('p')
     const createSummary = document.createElement('p')     
     
@@ -84,20 +71,24 @@ const displayMovieContent = async (optionId) => {
     mainDiv.appendChild(movieDiv)
  }
 
-
+const randomizeMovie = () => {
+    let randomIndex = Math.floor(Math.random() * 20)
+    return randomIndex
+}
 
 const postReview = () => {
     event.preventDefault()
 
     const selector = document.querySelector('#select-movie')
+    listValue = selector.options[selector.selectedIndex -1].text
+    selector.style.fontweight = listValue
+
     const reviewsList = document.querySelector('#reviews')
     const reviewInput = document.querySelector('#write-review')
     const reviewPost = document.createElement('li')
-    const formInput = reviewInput.value
 
-    reviewPost.innerText = selector.value + ': ' + formInput
+    reviewPost.innerText = selector.value + ': ' + reviewInput.value
     reviewsList.appendChild(reviewPost)
-    console.log(formInput.innerText)
      reviewInput.value = ''
    
 }
