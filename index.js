621319c3574f658e32f577aceebbb09357e06ecd
Scripter12 <elicardona57@gmail.com>
2019-10-01 20:27:01 -0400

    add choose random film button

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-  
+
   fetch("https://ghibliapi.herokuapp.com/films")
     .then(response => response.json())
-    .then(response => {
-      getFilms(response)
+    .then(filmsArr => {
+      getFilms(filmsArr)
     })
 
   let select = document.querySelector("select")
   select.addEventListener("change", (e) => {
-  
+
     let list = document.querySelector("ul");
- 
+
     let words = document.querySelector("#words")
     let submit = document.querySelector("#sub")
-    if (!e.target.value){
+    if (!e.target.value) {
       list.innerHTML = ""
       words.style.display = "none"
       submit.style.display = "none"
-    }else{
+    } else {
       list.innerHTML = ""
       words.style.display = "inline"
       submit.style.display = "inline"
       let i = 0;
-      while(i < listOfReviews[e.target.value].length){
-      list.appendChild(listOfReviews[e.target.value][i])
-      i++;}
+      while (i < listOfReviews[e.target.value].length) {
+        list.appendChild(listOfReviews[e.target.value][i])
+        i++;
+      }
     }
     words.value = ""
     let info = document.querySelector("#info")
-    let index = arrjson[e.target.selectedIndex]
-    fetch(`https://ghibliapi.herokuapp.com/films/${index}`)
+    fetch(`https://ghibliapi.herokuapp.com/films/${e.target.value}`)
       .then(response => response.json())
       .then(response => {
         while (info.firstChild) {
@@ -39,9 +39,11 @@ document.addEventListener("DOMContentLoaded", () => {
         let releaseYear = document.createElement("p")
         let description = document.createElement("p")
         title.innerText = response.title;
-        releaseYear.innerText = response.release_dateb
+        releaseYear.innerText = response.release_date
         description.innerText = response.description
-        if (index === null) {
+
+        if (!e.target.value) {
+
           title.innerText = "";
           releaseYear.innerText = ""
           description.innerText = ""
@@ -61,17 +63,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
 })
 
-let arrjson = [null];
+let filmsObj = {};
+
 let listOfReviews = {}
 
-function getFilms(film) {
+function getFilms(films) {
   let select = document.querySelector("select")
-  for (let i of film) {
+  for (let film of films) {
     let option = document.createElement("option")
-    option.innerText = i.title;
+    option.innerText = film.title;
+    option.value = film.id;
     select.appendChild(option)
-    arrjson.push(i.id)
-    listOfReviews[i.title] = [];
+    listOfReviews[film.id] = [];
+    filmsObj[film.id] = film;
   }
 }
 
@@ -80,21 +84,30 @@ function getFilms(film) {
 function postReview() {
   let words = document.querySelector("#words")
   let list = document.querySelector("ul")
-  let t = document.querySelector("h3")
+  let filmid = document.querySelector("select").value
   let bold = document.createElement("b");
-  bold.innerText = t.innerText;
+  bold.innerText = filmsObj[filmid].title;
   let review = document.createElement("li")
   review.appendChild(bold)
   let text = document.createTextNode(`: ${words.value}`)
   review.appendChild(text);
-
-  if (t.innerText && words.value) {
-    listOfReviews[t.innerText].push(review);
+  console.log()
+  if (filmid && words.value) {
+    listOfReviews[filmid].push(review);
     let i = 0;
-    while(i < listOfReviews[t.innerText].length){
-    list.appendChild(listOfReviews[t.innerText][i])
-    i++;
+    while (i < listOfReviews[filmid].length) {
+      list.appendChild(listOfReviews[filmid][i])
+      i++;
     }
   }
   words.value = ""
+}
+
+
+function rand() {
+  let select = document.querySelector("select");
+  let index = Math.floor(Math.random() * 19 + 1)
+  var event = new Event('change');
+  select.selectedIndex = index
+  select.dispatchEvent(event);
 }
