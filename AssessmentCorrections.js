6ba9e66917426917c1566d9d72926ef10729df23
Bpmauge <briahanamauge@pursuit.org>
2020-01-02 12:11:58 -0500

    fixed movie select with changing informations

--- AssessmentCorrections.js
+++ AssessmentCorrections.js
@@ -1,11 +1,15 @@
-/*Decided to retry the examine in a non stress enviornment 
+/*Decided to retry the exam in a non stress enviornment 
 // for me and I feel I am already working towards a better solutiion here.*/
 
 document.addEventListener("DOMContentLoaded", () => {
+    let movies = [];
+    let para = document.querySelector('div');
+
+
     let select = document.querySelector("#films");
     select.addEventListener('change', () => {
-        //console.log(select.value);
-        pickmovie()
+        para.innerText = '';
+        printList(select.value)
     })
 
     const createOption = (choice, index) => {
@@ -13,55 +17,51 @@ document.addEventListener("DOMContentLoaded", () => {
         option.setAttribute("id", `${choice}`/*`${index}`*/)
         option.innerText = choice;
         select.add(option)
-    } 
-     
-    //printList()
+
+    }
+
+
     let tests = document.querySelectorAll("#movie");
-    
-const pickmovie = (element) => {
-let someChoice = document.querySelectorAll('select');
-console.log(someChoice)
-// for(let choice of someChoice){
-    for (i = 0; i < someChoice.length; i++){
-    var choices = {
-       someChoice: `${someChoice.selectedindex}`
-    }
-}
-console.log(choices)
-let div = document.querySelector('div')
-let choiceIndex = someChoice.index;
-// div.appendChild.choiceIndex
-// div.innerHTML = someChoice[choiceIndex]
 
-}
+    const printList = (movie) => {
+        let movieTitles = [];
+        let index = 0;
+        let para = document.querySelector('div');
 
-// pickmovie()
-
-
-
-
-
-const printList = (movies) => {
-        let div = document.querySelector('div')
-        for (let item of movies) {
-             let items = document.createElement('p');
-             items.innerText = item.innerText
-            console.log("test:   ", items)
-              div.appendChild(items)
+        for (let i = 0; i < movies.length; i++) {
+            movieTitles.push(movies[i].name)
         }
+
+        if (movieTitles.includes(movie)) {
+            index = movieTitles.indexOf(movie)
+        }
+
+        var name = document.createElement('h3');
+        name.setAttribute('class', `${index}`)
+        name.innerHTML = movies[index].name
+        para.appendChild(name)
+        var year = document.createElement('p');
+        year.setAttribute('class', `${index}`)
+        year.innerHTML = movies[index].year
+        para.appendChild(year)
+        var info = document.createElement('p');
+        info.setAttribute('class', `${index}`)
+        info.innerHTML = movies[index].info
+        para.appendChild(info)
     }
-    const getMovie = () => {
+
+
+    const getMovie = (selected) => {
+        let selectedMovie = selected
         let movieLink = 'https://ghibliapi.herokuapp.com/films';
         axios.get(movieLink).then((response) => {
             try {
                 let movieList = response.data;
-                //let para = document.querySelector('div');
-                // var select = document.querySelector('#films')
+                let select = document.querySelector('#films')
                 let blank = document.createElement('option');
                 blank.innerText = 'Select a Movie';
                 select.add(blank);
-                console.dir(select);
-                var movies = [];
+
                 //create an object for the each movie to make it easier to click when selected from drop down menu
                 for (i = 0; i < movieList.length; i++) {
                     let movie = {
@@ -69,36 +69,19 @@ const printList = (movies) => {
                         year: `${movieList[i].release_date}`,
                         info: `${movieList[i].description}`
                     }
-                    // var name = document.createElement('h3');
-                    // name.setAttribute('class', `${i}`)
-                    // name.innerHTML = movieList[i].title
-                    // // para.appendChild(name)
-                    // var year = document.createElement('p');
-                    // year.setAttribute('class', `${i}`)
-                    // year.innerHTML = movieList[i].release_date
-                    // // para.appendChild(year)
-                    // var info = document.createElement('p');
-                    // info.setAttribute('class', `${i}`)
-                    // info.innerHTML = movieList[i].description
-                    // // para.appendChild(info)
-                    movies.push(movie);
-
-                createOption(`${movieList[i].title}`, `${i}`)
-                    // let option = document.createElement('option');
-                    // option.text = movieList[i].title
-                    // select.add(option);   
+                    movies.push(movie)
+                    createOption(`${movieList[i].title}`, `${i}`)
                 }
-                // printList(movie)
                 movies.unshift('');
-                console.log(movies)
-    
             } catch (err) {
                 console.log('oops, there was an error. please try again', err);
             }
-            
 
-    
-    })
-}
+        })
+    }
     getMovie()
+
 })
+
+
+
