3d06062510977b6549a84dc771bf542702d47166
Bpmauge <briahanamauge@pursuit.org>
2020-01-02 13:04:17 -0500

    added ablity to add reviews

--- AssessmentCorrections.js
+++ AssessmentCorrections.js
@@ -4,20 +4,42 @@
 document.addEventListener("DOMContentLoaded", () => {
     let movies = [];
     let para = document.querySelector('div');
+    let movieTitle = '';
+    movieTitle.className = 'movie-title';
 
 
     let select = document.querySelector("#films");
     select.addEventListener('change', () => {
         para.innerText = '';
-        printList(select.value)
+        printList(select.value);;;
+        movieTitle = select.value;;;
     })
+
+    let form = document.querySelector("form");
+    form.addEventListener('submit', (event) => {
+
+        event.preventDefault();
+        makeReview(movieTitle);;;
+    })
+
 
     const createOption = (choice, index) => {
         let option = document.createElement('option');
-        option.setAttribute("id", `${choice}`/*`${index}`*/)
+        option.setAttribute("id", `${choice}`/*`${index}`*/);;;
         option.innerText = choice;
-        select.add(option)
+        select.add(option);;;
 
+    }
+
+    const makeReview = (movieTitle) => {
+        let inputBox = document.querySelector('#review').value;;;
+        inputBox.className = 'review-text';;;
+        let reviewUL = document.querySelector('#review-list');;;
+        console.log(inputBox);;;
+        console.log(movieTitle, inputBox);;;
+        let reviewLI = document.createElement('li');
+        reviewLI.innerText = `${movieTitle}: ${inputBox}`;;;
+        reviewUL.append(reviewLI);;;
     }
 
 
@@ -29,25 +51,25 @@ document.addEventListener("DOMContentLoaded", () => {
         let para = document.querySelector('div');
 
         for (let i = 0; i < movies.length; i++) {
-            movieTitles.push(movies[i].name)
+            movieTitles.push(movies[i].name);;
         }
 
         if (movieTitles.includes(movie)) {
-            index = movieTitles.indexOf(movie)
+            index = movieTitles.indexOf(movie);;
         }
 
         var name = document.createElement('h3');
-        name.setAttribute('class', `${index}`)
-        name.innerHTML = movies[index].name
-        para.appendChild(name)
+        name.setAttribute('class', `${index}`);;
+        name.innerHTML = movies[index].name;;
+        para.appendChild(name);;
         var year = document.createElement('p');
-        year.setAttribute('class', `${index}`)
-        year.innerHTML = movies[index].year
-        para.appendChild(year)
+        year.setAttribute('class', `${index}`);;
+        year.innerHTML = movies[index].year;;
+        para.appendChild(year);;
         var info = document.createElement('p');
-        info.setAttribute('class', `${index}`)
-        info.innerHTML = movies[index].info
-        para.appendChild(info)
+        info.setAttribute('class', `${index}`);;
+        info.innerHTML = movies[index].info;;
+        para.appendChild(info);;
     }
 
 
@@ -57,9 +79,11 @@ document.addEventListener("DOMContentLoaded", () => {
         axios.get(movieLink).then((response) => {
             try {
                 let movieList = response.data;
-                let select = document.querySelector('#films')
+                let select = document.querySelector('#films');
                 let blank = document.createElement('option');
                 blank.innerText = 'Select a Movie';
+                blank.setAttribute('selected', 'true');
+                blank.setAttribute('disabled', 'disabled');
                 select.add(blank);
 
                 //create an object for the each movie to make it easier to click when selected from drop down menu
@@ -70,7 +94,7 @@ document.addEventListener("DOMContentLoaded", () => {
                         info: `${movieList[i].description}`
                     }
                     movies.push(movie)
-                    createOption(`${movieList[i].title}`, `${i}`)
+                    createOption(`${movieList[i].title}`, `${i}`);
                 }
                 movies.unshift('');
             } catch (err) {
@@ -79,7 +103,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         })
     }
-    getMovie()
+    getMovie();
 
 })
 
